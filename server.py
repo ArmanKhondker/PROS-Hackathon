a5580016e0fb8336c928e072bfca36c128ed3a37
Matthew Mutammara <Matthew@jsweeney-pc1.prosrm.com>
2018-07-06 11:57:18 -0500

    Integrated sentiment score with server. Correctly sends data to frontend, but doesnt display on frontend as is.

--- server.py
+++ server.py
@@ -5,9 +5,15 @@ from datetime import timedelta
 from flask import Flask, request, jsonify, make_response, current_app
 from functools import update_wrapper
 from predict import boost_predict
+from loop import loop
 import pickle
 
 app = Flask(__name__)
+videos = {'Canon': ['q1ERQ92k3mk', 'e7zDuWjC4gQ', 'XX6eO8ivkq0', 'r4pZQepv4r4', 'AXuM7mw9gyg', 'wOIuZ1kqOcE', 'W71YkswXg3I', 'K_z3dIKqSuQ', 'btaesOmA_ro', 'FflMjrzBS9E'],
+            'Nikon': ['06B0BeAo2E0', 'Z3a9RHaDFF0', 'cugK3f6Q6cQ', 'xH5s1nmbBfc', 'io3MWcwXjJg', '0uPSmGxhNWQ', 'A_OMIRHXynQ', 'p36JKnSOBLY', '8NIi5b791GU', 'utKjjzTaQoQ'],
+            'Sony': ['jDcB36B7HDM', 'oqtxVSlxM90', 'AxRbtI5EeF0', 'NROsTlGeqB8', 'cx20nTPZjes', 'aCEkhwAM7T8', 'ry0uTWJf970', 'M7qnb69nVgw', 'wUJc83wV6iA', '8P5irosYUnA'],
+            'Panasonic': ['wbLpH5TewnM', '6SQdPxFqUp4', 'cJKRgMPk1BU', 'xEpuGf9hD4w', 'juBkGij2rEs', 'G4485EViAVA', 'Xv2OWv6pWVM', 'yGgJlToYVFc', 'Hs3dkdtKimQ', 'NXdIHAgOB7A'],
+            'Olympus': ['eWsSSFQCP4o', 'BP3tznYjMlg', 'VwnCAz3WtRg', '1FVD5kM9F2w', 'ioKpVCsEfhE', 'SpYrIqz09kU', 'E72u0hHWhlU', '0uQKYgQaNHQ', 'WMg5BZbyMo4', 'IfLL9qmY2Ic']}
 
 def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
     if methods is not None:
@@ -57,7 +63,7 @@ def boost():
 	out['price'] = boost_predict(model,args['model'],args['releaseDate'],args['maxRes'],args['lowRes'],
 		args['effPix'],args['zoomW'],args['zoomT'],args['normalFocus'],args['macroFocus'],
 		args['storage'],args['weight'],args['dimension'])[0]
-	out['favor'] = 0.5
+	out['sentimentScore'] = loop(videos[args['model']])
 	return jsonify(out)
 
 if __name__ == '__main__':
